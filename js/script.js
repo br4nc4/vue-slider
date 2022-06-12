fe5306764451f8a5bd64de58e641ddbd7cae4564
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-12 20:07:23 +0200

    vue slider incompleted pt.2 12-06

--- js/script.js
+++ js/script.js
@@ -2,37 +2,44 @@ const newApp = new Vue({
     el: "#app",
 
     data:{
-
         slides: [
             {
-                image: 'img/01.jpg',
+                src: 'img/01.jpg',
                 title: 'Svezia',
                 text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                id: 1,
             },
             {
-                image: 'img/02.jpg',
+                src: 'img/02.jpg',
                 title: 'Svizzera',
                 text: 'Lorem ipsum.',
+                id: 2,
             },
             {
-                image: 'img/03.jpg',
+                src: 'img/03.jpg',
                 title: 'Gran Bretagna',
                 text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                id: 3,
             },
             {
-                image: 'img/04.jpg',
+                src: 'img/04.jpg',
                 title: 'Germania',
                 text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                id: 4,
             },
             {
-                image: 'img/05.jpg',
+                src: 'img/05.jpg',
                 title: 'Paradise',
                 text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+                id: 5,
             }
-        ],
-        
-    
-
+        ],   
     }
-})
+});
+
+
+
+
+
+
 
